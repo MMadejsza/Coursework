258e7f5599ada9bf91132cd7d87dd7ea0f22fb07
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-04-11 17:25:57 +0100

    Resources added

--- CW1.cpp
+++ CW1.cpp
@@ -7,3 +7,7 @@ int main(int argc, char **argv)
     cout << "hello";
     return 0;
 }
+// ? Resources:
+// ? https://developer.mozilla.org/en-US/
+// ? https://www.w3schools.com/
+// ? https://stackoverflow.com/
